d8af3965f0812bb61d8b0be099ff2289463c532f
badjavii <javierandresotero.2804@gmail.com>
2025-06-23 20:38:24 -0400

    instructions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #include "sha256.h"
 using namespace std;
 
+// PARA COPIA N = 1
+// 1- Copiar el contenido original.txt en copia1.txt
+// 2- Encriptar copia1.txt
+// 3- Sacar hash de copia1.txt
+// 4- Sacar otro hash de copia1.txt
+// 5- Comparar los hash
+// 6- Desencriptar copia1.txt en otro archivo d_copia1.txt
+// 7- Comparar el contenido de d_copia1.txt con original.txt
+
 int main()
 {
     const char *mensaje = "Hola Mundo";
